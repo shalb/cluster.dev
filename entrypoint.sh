feb1070323adbfa42498107dcaba2935170b9275
Volodymyr Tsap <voa@shalb.com>
2019-12-23 21:30:59 +0000

    new entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,24 @@
-#!/bin/sh -l
-
+#!/bin/bash
+# Parse YAML configs in .cluster-dev/*
 echo "Hello $1"
 time=$(date)
-find .
+
+source ./bin/yaml.sh
+
+for CLUSTER_MANIFEST_FILE in $(find ./.cluster.dev/ -type f); do 
+parse_yaml $CLUSTER_MANIFEST_FILE
+create_variables $CLUSTER_MANIFEST_FILE
+
+case $cluster_provisioner_type in
+minikube)
+echo "AWS Mikikube Password:" $cluster_clould_awsPassword
+echo "terraform init && terraform plan" 
+;;
+eks)
+echo "AWS EKS Password:" $cluster_clould_awsPassword
+;;
+esac
+
+done
+
 echo ::set-output name=time::$time
