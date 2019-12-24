4aae5d3b179c9fbca06ce82f6b176251873026d3
Volodymyr Tsap <voa@shalb.com>
2019-12-24 11:56:04 +0000

    test tf run

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Create Complete Kubernetes dev Enviornment
+## Deploy Cluster
+
+Create Kubernetes Cluster Based on manifest:
+```yaml
+cluster:
+  name: minikube-one
+  provider:
+    minikube:             
+      aws:
+        instanceType: "m4.large"
+        awsUser: "AWSPROGRAMMATICUSER"
+        awsPassword: ${git.awspassword}
+```
+Cluster should be deployed by adding `.cluster.dev/minikube-one.yaml` into repository
+
+In background there should be: 
+ - Terraform module which creates Minikube instance with AWS Account credentials
+ - Produced kubeconfig should be generated and passed to value into target git repo credentials
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # Parse YAML configs in .cluster-dev/*
-echo "Hello $1"
-time=$(date)
+
+CLUSTER_CONFIG_PATH=$1
+CLOUD_USER=$2
+CLOUD_PASS=$3
+
+echo "Starting job with arguments CLUSTER_CONFIG_PATH: $CLUSTER_CONFIG_PATH, CLOUD_USER: $CLOUD_USER" 
 
 source ./bin/yaml.sh
 
-for CLUSTER_MANIFEST_FILE in $(find ./.cluster.dev/ -type f); do 
+for CLUSTER_MANIFEST_FILE in $(find $CLUSTER_CONFIG_PATH -type f); do 
 parse_yaml $CLUSTER_MANIFEST_FILE
 create_variables $CLUSTER_MANIFEST_FILE
 
 case $cluster_provisioner_type in
 minikube)
 echo "AWS Mikikube Password:" $cluster_clould_awsPassword
-echo "terraform init && terraform plan" 
+terraform init
 ;;
 eks)
 echo "AWS EKS Password:" $cluster_clould_awsPassword
