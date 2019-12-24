0180990582f93d5f611d1c6a6f8980659de51c41
Volodymyr Tsap <voa@shalb.com>
2019-12-24 22:26:49 +0000

    terraform init test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Parse YAML configs in .cluster-dev/*
+source ./bin/yaml.sh
 
 CLUSTER_CONFIG_PATH=$1
 CLOUD_USER=$2
@@ -7,23 +8,45 @@ CLOUD_PASS=$3
 
 echo "Starting job in repo: $GITHUB_REPOSITORY with arguments CLUSTER_CONFIG_PATH: $CLUSTER_CONFIG_PATH, CLOUD_USER: $CLOUD_USER" 
 
-source ./bin/yaml.sh
+for CLUSTER_MANIFEST_FILE in $(find $CLUSTER_CONFIG_PATH -type f); do
 
-for CLUSTER_MANIFEST_FILE in $(find $CLUSTER_CONFIG_PATH -type f); do 
 parse_yaml $CLUSTER_MANIFEST_FILE
 create_variables $CLUSTER_MANIFEST_FILE
 
-case $cluster_clould_provider in
+case $cluster_cloud_provider in
 aws)
-echo "Cloud Provider AWS" 
-terraform init 
+
+case $cluster_cloud_provisioner in 
+minikube)
+cd terraform/aws/minikube/ && ls -las
+echo "Cloud Provider AWS. Provisioner: Minikube" 
+echo terraform init \
+echo -backend-config="bucket=$GITHUB_REPOSITORY" \
+echo -backend-config="$cluster_name/terraform.state" \
+echo -backend-config="region=$cluster_cloud_region"
+pwd
+terraform init  \
+-backend-config="bucket=$GITHUB_REPOSITORY" \
+-backend-config="$cluster_name/terraform.state" \
+-backend-config="region=$cluster_cloud_region"
+-backend-config="access_key=$CLOUD_USER"
+-backend-config="secret_key=$CLOUD_PASS"
 ;;
+
+eks)
+echo "Cloud Provider AWS. Provisioner: EKS" 
+;;
+esac
+;;
+
 gcp)
 echo "Cloud Provider Google"
 ;;
+
 azure)
 echo "Cloud Provider Azure"
 ;;
+
 esac
 
 done
