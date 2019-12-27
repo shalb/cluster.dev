8b4b1276c201913ba3ad4d02ca7cf97d92dc6673
Volodymyr Tsap <voa@shalb.com>
2019-12-27 22:15:08 +0000

    fix naming

--- terraform/aws/backend/variables.tf
+++ terraform/aws/backend/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  type        = "string"
+  type        = string
   description = "The AWS region."
 }
 
 variable "s3_backend_bucket" {
-  type        = "string"
+  type        = string
   description = "The s3 backend bucket"
 }
