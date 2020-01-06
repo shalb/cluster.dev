7c386a5e170de1a6a5bef1a405b9468ae196a710
Volodymyr Tsap <voa@shalb.com>
2020-01-06 15:08:06 +0000

    fix wget

--- terraform/aws/minikube/k8s-userdata.tpl.sh
+++ terraform/aws/minikube/k8s-userdata.tpl.sh
@@ -3,10 +3,11 @@ echo "test" > /tmp/test-file.txt
 pwd
 
 # Install AWS utils
+yum install -y wget
 cd /usr/local/bin
 wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
 unzip ec2-api-tools.zip
 mv ec2-api-tools-* /usr/local/bin/ec2-api-tools
 
 # execute listing for s3
-aws s3 ls
+aws s3 ls > /root/aws-s3-output.txt
