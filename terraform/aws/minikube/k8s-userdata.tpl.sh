4b999c9e3bc93f4cfc584b9d3794d047c1704ec7
Volodymyr Tsap <voa@shalb.com>
2020-01-06 15:00:40 +0000

    aws s3 policy check

--- terraform/aws/minikube/k8s-userdata.tpl.sh
+++ terraform/aws/minikube/k8s-userdata.tpl.sh
@@ -1,3 +1,12 @@
 touch /tmp/test-file.txt
 echo "test" > /tmp/test-file.txt
 pwd
+
+# Install AWS utils
+cd /usr/local/bin
+wget http://s3.amazonaws.com/ec2-downloads/ec2-api-tools.zip
+unzip ec2-api-tools.zip
+mv ec2-api-tools-* /usr/local/bin/ec2-api-tools
+
+# execute listing for s3
+aws s3 ls
