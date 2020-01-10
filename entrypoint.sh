44ce37457b5e7300fb195acc232c00d1bcd645fe
Volodymyr Tsap <voa@shalb.com>
2020-01-10 19:27:09 +0000

    Fix s3 upload files

--- entrypoint.sh
+++ entrypoint.sh
@@ -107,11 +107,13 @@ terraform apply -auto-approve -compact-warnings \
                   -var="hosted_zone=$cluster_cloud_domain"
 
 # Apply output for user
-echo -e "*** Download your kubeconfig using command: aws s3 cp s3://${CLUSTER_FULLNAME}/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/kubeconfig_${CLUSTER_FULLNAME} \n 
-             export KUBECONFIG=\$KUBECONFIG:~/.kube/kubeconfig_${CLUSTER_FULLNAME} \n
+echo -e "*** Download and apply your kubeconfig using commands: 
+             aws s3 cp s3://${CLUSTER_FULLNAME}/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/kubeconfig_${CLUSTER_FULLNAME} 
+             export KUBECONFIG=\$KUBECONFIG:~/.kube/kubeconfig_${CLUSTER_FULLNAME}
              kubectl get ns"
 
-echo -e "*** Download your bastion ssh key using command: aws s3 cp s3://${CLUSTER_FULLNAME}/id_rsa_${cluster_name}.pem ~/.ssh/id_rsa_${cluster_name}.pem \n
+echo -e "*** Download your bastion ssh key using commands: 
+             aws s3 cp s3://${CLUSTER_FULLNAME}/id_rsa_${cluster_name}.pem ~/.ssh/id_rsa_${cluster_name}.pem
              ssh -i ~/.ssh/id_rsa_${cluster_name}.pem centos@$CLUSTER_FULLNAME.$cluster_cloud_domain"
 
 ;; # end of minikube
