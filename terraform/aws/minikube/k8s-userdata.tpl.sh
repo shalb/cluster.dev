7344afb575f1dac040a418d6d24374dcd0f44f6a
Volodymyr Tsap <voa@shalb.com>
2020-01-12 12:50:19 +0000

    edit key pass

--- terraform/aws/minikube/k8s-userdata.tpl.sh
+++ terraform/aws/minikube/k8s-userdata.tpl.sh
@@ -7,8 +7,7 @@ export PATH=\$$PATH:/usr/local/bin
 aws s3 cp /home/centos/kubeconfig s3://${cluster_name}/kubeconfig_${cluster_name}
 
 # copy private ssh key to s3
-cat << EOF > /home/centos/.ssh/id_rsa 
-${private_key}
+cat << EOF > /home/centos/.ssh/id_rsa
 EOF
 chmod 600 /home/centos/.ssh/id_rsa
 aws s3 cp /home/centos/.ssh/id_rsa  s3://${cluster_name}/id_rsa_${cluster_name}.pem
