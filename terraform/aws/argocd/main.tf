8cf73b9f1875a5fee7d32b86eac6c67984436cfc
Volodymyr Tsap <voa@shalb.com>
2020-01-20 16:06:52 +0000

    Create k8s service accounts

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -1,6 +1,32 @@
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "tiller"
+    namespace = "kube-system"
+  }
+}
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+        name = "tiller"
+  }
+  subject {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "User"
+    name      = "system:serviceaccount:kube-system:tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind  = "ClusterRole"
+    name = "cluster-admin"
+  }
+  depends_on = ["kubernetes_service_account.tiller"]
+}
+
 provider "helm" {
-  version        = "~> 0.10"
-  install_tiller = true
+    tiller_image = "gcr.io/kubernetes-helm/tiller:v2.12.3"
+    install_tiller = true
+    service_account = "tiller"
+    namespace = "kube-system"
 }
 
 data "helm_repository" "argo" {
