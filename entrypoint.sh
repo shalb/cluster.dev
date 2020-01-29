21b4d56d45e889df8a4d4ed580ebf90cee521902
Volodymyr Tsap <voa@shalb.com>
2020-01-29 21:29:06 +0000

    Test .kube/config

--- entrypoint.sh
+++ entrypoint.sh
@@ -113,13 +113,13 @@ function pull_kubeconfig {
   WAIT_TIMEOUT=5;
   until kubectl version --request-timeout=5s > /dev/null; do  
       aws s3 cp s3://${CLUSTER_FULLNAME}/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/kubeconfig_${CLUSTER_FULLNAME} 
-      export KUBECONFIG=~/.kube/kubeconfig_${CLUSTER_FULLNAME}
       cp ~/.kube/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/config > /dev/null
       echo "*** Waiting $WAIT_TIMEOUT seconds for Kubernetes Cluster gets ready"; sleep $WAIT_TIMEOUT; 
   done
 }
 
 pull_kubeconfig
+
 # Create varialbe for changes in kubeconfig to trigger redeployment for dependent resources (ex ArgoCD)
 KUBECONFIG_HASH=$(md5sum ~/.kube/kubeconfig_${CLUSTER_FULLNAME} | cut -c 1-8)
 
