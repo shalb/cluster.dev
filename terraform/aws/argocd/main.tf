1f3ae7d1936e7356d928899d2a4cf8f20d76cac7
Volodymyr Tsap <voa@shalb.com>
2020-01-29 22:44:56 +0000

    define kubeconfig to provider config

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -4,6 +4,30 @@ provider "helm" {
   service_account = kubernetes_service_account.tiller.metadata.0.name
   namespace = kubernetes_service_account.tiller.metadata.0.namespace
   tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+data "helm_repository" "argo" {
+  name = "argo"
+  url  = "https://argoproj.github.io/argo-helm"
+}
+
+resource "helm_release" "argo-cd" {
+  name       = "argo-cd"
+  repository = data.helm_repository.argo.metadata[0].name
+  chart      = "argo-cd"
+  version    = "1.6.3"
+  namespace  = "argocd"
+
+  values = [
+    "${file("values.yaml")}"
+  ]
 }
 
 resource "kubernetes_service_account" "tiller" {
@@ -34,21 +58,4 @@ resource "kubernetes_cluster_role_binding" "tiller" {
     namespace = "kube-system"
   }
 
-}
-
-data "helm_repository" "argo" {
-  name = "argo"
-  url  = "https://argoproj.github.io/argo-helm"
-}
-
-resource "helm_release" "argo-cd" {
-  name       = "argo-cd"
-  repository = data.helm_repository.argo.metadata[0].name
-  chart      = "argo-cd"
-  version    = "1.6.3"
-  namespace  = "argocd"
-
-  values = [
-    "${file("values.yaml")}"
-  ]
 }
