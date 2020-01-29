61d919ac251e8a28de9055f13f285e44a6003172
Volodymyr Tsap <voa@shalb.com>
2020-01-29 13:07:48 +0000

    Add Ingress to Minikube Instance

--- terraform/aws/minikube/main.tf
+++ terraform/aws/minikube/main.tf
@@ -39,6 +39,7 @@ module "minikube" {
     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/metrics-server.yaml",
     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/dashboard.yaml",
     "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/external-dns.yaml",
+    "https://raw.githubusercontent.com/scholzj/terraform-aws-minikube/master/addons/ingress.yaml",
   ]
   
 }
