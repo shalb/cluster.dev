af03b9b402b327195b182798a677f3546a444387
Volodymyr Tsap <voa@shalb.com>
2020-01-30 15:27:34 +0000

    Return out of module

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -1,8 +1,72 @@
-variable "kubeconfig" {
-  description = ""
+resource "null_resource" "kubeconfig_update" {
+  triggers = {
+    policy_sha1 = "${sha1(file("~/.kube/config"))}"
+  }
 }
 
-module "argocd" { 
-  source =  "./module"
-  kubeconfig = var.kubeconfig
+provider "helm" {
+  install_tiller = true
+  version = "~> 0.10.0"
+  service_account = kubernetes_service_account.tiller.metadata.0.name
+  namespace = kubernetes_service_account.tiller.metadata.0.namespace
+  tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
 }
+
+data "helm_repository" "argo" {
+  name = "argo"
+  url  = "https://argoproj.github.io/argo-helm"
+}
+
+resource "helm_release" "argo-cd" {
+  name       = "argo-cd"
+  repository = data.helm_repository.argo.metadata[0].name
+  chart      = "argo-cd"
+  version    = "1.6.3"
+  namespace  = "argocd"
+
+  values = [
+    "${file("values.yaml)}"
+  ]
+  depends_on = [
+    kubernetes_service_account.tiller,
+  ]
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "terraform-tiller"
+    namespace = "kube-system"
+  }
+  automount_service_account_token = true
+
+  depends_on = [
+    null_resource.kubeconfig_update,
+  ]
+
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "terraform-tiller"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = "terraform-tiller"
+
+    api_group = ""
+    namespace = "kube-system"
+  }
+
+  depends_on = [
+    null_resource.kubeconfig_update,
+  ]
+
+}
+
--- terraform/aws/argocd/module/main.tf
+++ terraform/aws/argocd/module/main.tf
@@ -10,18 +10,11 @@ provider "helm" {
   service_account = kubernetes_service_account.tiller.metadata.0.name
   namespace = kubernetes_service_account.tiller.metadata.0.namespace
   tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
-  kubernetes {
-    config_path = "~/.kube/config"
-  }
 }
 
 data "helm_repository" "argo" {
   name = "argo"
   url  = "https://argoproj.github.io/argo-helm"
-}
-
-provider "kubernetes"{
-  config_path = "~/.kube/config"
 }
 
 resource "helm_release" "argo-cd" {
