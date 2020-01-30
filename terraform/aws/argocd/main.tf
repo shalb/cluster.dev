abb173c46a3904d730396d275c9598da2a7bf1a3
Volodymyr Tsap <voa@shalb.com>
2020-01-30 12:35:11 +0000

    Minor fix for module

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -1,3 +1,7 @@
+variable "kubeconfig_hash" {
+  description = "Hash from kubeconfig to re-create Tiller and Argo when it changed"
+}
+
 module "argocd" { 
   source =  "./module"
   kubeconfig_hash = var.kubeconfig_hash
