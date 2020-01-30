c8121a310a8a566bafd375ead743c2448f4d65f3
Volodymyr Tsap <voa@shalb.com>
2020-01-30 15:46:26 +0000

    chanse sequence

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -4,34 +4,6 @@ resource "null_resource" "kubeconfig_update" {
   }
 }
 
-provider "helm" {
-  install_tiller = true
-  version = "~> 0.10.0"
-  service_account = kubernetes_service_account.tiller.metadata.0.name
-  namespace = kubernetes_service_account.tiller.metadata.0.namespace
-  tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
-}
-
-data "helm_repository" "argo" {
-  name = "argo"
-  url  = "https://argoproj.github.io/argo-helm"
-}
-
-resource "helm_release" "argo-cd" {
-  name       = "argo-cd"
-  repository = data.helm_repository.argo.metadata[0].name
-  chart      = "argo-cd"
-  version    = "1.6.3"
-  namespace  = "argocd"
-
-  values = [
-    "${file("values.yaml")}"
-  ]
-  depends_on = [
-    kubernetes_service_account.tiller,
-  ]
-}
-
 resource "kubernetes_service_account" "tiller" {
   metadata {
     name = "terraform-tiller"
@@ -70,3 +42,30 @@ resource "kubernetes_cluster_role_binding" "tiller" {
 
 }
 
+provider "helm" {
+  install_tiller = true
+  version = "~> 0.10.0"
+  service_account = kubernetes_service_account.tiller.metadata.0.name
+  namespace = kubernetes_service_account.tiller.metadata.0.namespace
+  tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
+}
+
+data "helm_repository" "argo" {
+  name = "argo"
+  url  = "https://argoproj.github.io/argo-helm"
+}
+
+resource "helm_release" "argo-cd" {
+  name       = "argo-cd"
+  repository = data.helm_repository.argo.metadata[0].name
+  chart      = "argo-cd"
+  version    = "1.6.3"
+  namespace  = "argocd"
+
+  values = [
+    "${file("values.yaml")}"
+  ]
+  depends_on = [
+    kubernetes_service_account.tiller,
+  ]
+}
