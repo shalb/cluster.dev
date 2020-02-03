e5c3b9f65bf2fe2381e3b3bc5d12297d3ac682ff
Volodymyr Tsap <voa@shalb.com>
2020-02-03 16:20:23 +0000

    Fix provider

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -9,5 +9,48 @@ provider "helm" {
 module "argocd" {
   source =  "./module"
   kubeconfig = "~/.kube/config"
+}
+
+resource "null_resource" "kubeconfig_update" {
+  triggers = {
+    policy_sha1 = "${sha1(file("~/.kube/config"))}"
+  }
+}
+
+resource "kubernetes_service_account" "tiller" {
+  metadata {
+    name = "terraform-tiller"
+    namespace = "kube-system"
+  }
+  automount_service_account_token = true
+
+  depends_on = [
+    null_resource.kubeconfig_update
+  ]
+
+}
+
+resource "kubernetes_cluster_role_binding" "tiller" {
+  metadata {
+    name = "terraform-tiller"
+  }
+
+  role_ref {
+    kind = "ClusterRole"
+    name = "cluster-admin"
+    api_group = "rbac.authorization.k8s.io"
+  }
+
+  subject {
+    kind = "ServiceAccount"
+    name = "terraform-tiller"
+
+    api_group = ""
+    namespace = "kube-system"
+  }
+
+  depends_on = [
+    null_resource.kubeconfig_update
+  ]
 
 }
--- terraform/aws/argocd/module/main.tf
+++ terraform/aws/argocd/module/main.tf
@@ -20,44 +20,6 @@ resource "helm_release" "argo-cd" {
     "${file("${path.module}/values.yaml")}"
   ]
   depends_on = [
-    kubernetes_service_account.tiller,
-  ]
-}
-
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name = "terraform-tiller"
-    namespace = "kube-system"
-  }
-  automount_service_account_token = true
-
-  depends_on = [
-    null_resource.kubeconfig_update
-  ]
-
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "terraform-tiller"
-  }
-
-  role_ref {
-    kind = "ClusterRole"
-    name = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-
-  subject {
-    kind = "ServiceAccount"
-    name = "terraform-tiller"
-
-    api_group = ""
-    namespace = "kube-system"
-  }
-
-  depends_on = [
-    null_resource.kubeconfig_update
-  ]
-
+    null_resource.kubeconfig_update,
+      ]
 }
