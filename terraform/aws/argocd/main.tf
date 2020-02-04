5ee93e4dc3b0190fe8cae609ca9218674ae806ab
Volodymyr Tsap <voa@shalb.com>
2020-02-04 17:05:59 +0000

    Push change

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -2,7 +2,7 @@ provider "helm" {
   install_tiller = true
   version = "~> 0.10.0"
   service_account = kubernetes_service_account.tiller.metadata.0.name
-  namespace = "kube-system"
+  namespace = kubernetes_service_account.tiller.metadata.0.namespace
   tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
 }
 
