b4599e045738eade5b7e1b25c099a8b6f61d1a4b
Volodymyr Tsap <voa@shalb.com>
2020-02-06 09:00:38 +0000

    Test Helm3

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -1,9 +1,5 @@
 provider "helm" {
-  install_tiller = true
-  version = "~> 0.10.0"
-  service_account = kubernetes_service_account.tiller.metadata.0.name
-  namespace = kubernetes_service_account.tiller.metadata.0.namespace
-  tiller_image = "gcr.io/kubernetes-helm/tiller:v2.14.1"
+  version = "~> 1.0.0"
 }
 
 # Check for kubeconfig and update resources when cluster gets re-created.
@@ -11,45 +7,6 @@ resource "null_resource" "kubeconfig_update" {
   triggers = {
     policy_sha1 = "${sha1(file("~/.kube/config"))}"
   }
-}
-
-# Provise tiller SA
-resource "kubernetes_service_account" "tiller" {
-  metadata {
-    name = "terraform-tiller"
-    namespace = "kube-system"
-  }
-  automount_service_account_token = true
-
-  depends_on = [
-    null_resource.kubeconfig_update
-  ]
-
-}
-
-resource "kubernetes_cluster_role_binding" "tiller" {
-  metadata {
-    name = "terraform-tiller"
-  }
-
-  role_ref {
-    kind = "ClusterRole"
-    name = "cluster-admin"
-    api_group = "rbac.authorization.k8s.io"
-  }
-
-  subject {
-    kind = "ServiceAccount"
-    name = "terraform-tiller"
-
-    api_group = ""
-    namespace = "kube-system"
-  }
-
-  depends_on = [
-    null_resource.kubeconfig_update
-  ]
-
 }
 
 # Deploy ArgoCD 
@@ -72,4 +29,3 @@ resource "helm_release" "argo-cd" {
     null_resource.kubeconfig_update,
       ]
 }
-
