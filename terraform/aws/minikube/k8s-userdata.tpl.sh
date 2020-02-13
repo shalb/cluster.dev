7da2855109d70c30f36a6a6ab31786615f00a8ec
romanprog <roman.progonnyj@gmail.com>
2020-02-13 15:50:04 +0200

    use new minilube module on ubuntu18 ami

--- terraform/aws/minikube/k8s-userdata.tpl.sh
+++ terraform/aws/minikube/k8s-userdata.tpl.sh
@@ -1,14 +1,14 @@
 # Install AWS utils
-yum install -y wget unzip python3-pip.noarch
+apt-get update
+apt-get install -y wget unzip python3-pip
 pip3 install awscli
 export PATH=\$$PATH:/usr/local/bin
 
-# copy kubeconfig to s3
-aws s3 cp /home/centos/kubeconfig s3://${cluster_name}/kubeconfig_${cluster_name}
+#copy kubeconfig to s3
+aws s3 cp /home/ubuntu/kubeconfig s3://${cluster_name}/kubeconfig_${cluster_name}
 
-# copy private ssh key to s3
-cat << EOF > /home/centos/.ssh/id_rsa 
-${private_key}
+copy private ssh key to s3
+cat << EOF > /home/ubuntu/.ssh/id_rsa
 EOF
-chmod 600 /home/centos/.ssh/id_rsa
-aws s3 cp /home/centos/.ssh/id_rsa  s3://${cluster_name}/id_rsa_${cluster_name}.pem
+chmod 600 /home/ubuntu/.ssh/id_rsa
+aws s3 cp /home/ubuntu/.ssh/id_rsa  s3://${cluster_name}/id_rsa_${cluster_name}.pem
