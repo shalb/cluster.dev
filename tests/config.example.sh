8815d40506fa83ee6c557d44fde58749d36959f0
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2020-02-27 16:23:25 +0200

    Fix hardcode

--- tests/config.example.sh
+++ tests/config.example.sh
@@ -2,3 +2,4 @@
 
 AWS_SECRET_KEY="" # aws_secret_access_key
 AWS_SECRET_TOKEN="" # aws_access_key_id
+GH_ACTION_WORKFLOW_PATH="./.cluster.dev/gh-5.yaml" # Relative path from project root
--- tests/tests.sh
+++ tests/tests.sh
@@ -11,10 +11,11 @@ readonly DOCKER_IMAGE_NAME="cluster.dev:${GIT_SHOT_COMMIT}-local-tests"
 
 docker build -t ${DOCKER_IMAGE_NAME} .
 
-# Get from vonfig.sh
+# Get from config.sh
 readonly USER="${AWS_SECRET_KEY}"
 readonly PASS="${AWS_SECRET_TOKEN}"
+readonly WORKFLOW_PATH="${GH_ACTION_WORKFLOW_PATH}"
 
 docker run --name NAME --workdir /github/workspace --rm -v "${SRC_PATH}":"/github/workspace" \
            -e GITHUB_REPOSITORY="shalb" \
-           ${DOCKER_IMAGE_NAME}  "./.cluster.dev/gh-5.yaml" "${USER}" "${PASS}"
+           ${DOCKER_IMAGE_NAME} "${WORKFLOW_PATH}" "${USER}" "${PASS}"
