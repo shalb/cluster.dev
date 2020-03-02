e86e59a40627894456a1a75ba9741c7bbe8ec964
Maksym Vlasov <MaxymVlasov@users.noreply.github.com>
2020-03-02 17:14:55 +0200

    Update config.example.sh

--- tests/config.example.sh
+++ tests/config.example.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-AWS_SECRET_KEY="" # aws_secret_access_key
 AWS_SECRET_TOKEN="" # aws_access_key_id
+AWS_SECRET_KEY="" # aws_secret_access_key
 GH_ACTION_WORKFLOW_PATH="./.cluster.dev/gh-5.yaml" # Relative path from project root
