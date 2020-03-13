b87b27f77f6bef593b15123cea14f110be5ff771
romanprog <roman.progonnyj@gmail.com>
2020-03-13 18:33:32 +0200

    code style

--- tests/config.example.sh
+++ tests/config.example.sh
@@ -2,6 +2,6 @@
 
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY=""
-# Timeout in seconds.
-ACTION_TIMEOUT="600"
+
+ACTION_TIMEOUT="600" # Timeout in seconds. After timeout action will be terminated container will be stopped.
 GH_ACTION_WORKFLOW_PATH="./.cluster.dev/gh-7.yaml" # Relative path from project root
--- tests/tests.sh
+++ tests/tests.sh
@@ -6,13 +6,13 @@
 . config.sh
 
 readonly SRC_PATH=$(realpath $(dirname $(readlink -f $0))/../)
-cd ${SRC_PATH}
-source ${SRC_PATH}/bin/bash-logger.sh
+cd "${SRC_PATH}"
+source "${SRC_PATH}/bin/bash-logger.sh"
 
-readonly GIT_SHORT_COMMIT=$(git rev-parse --short HEAD)
+readonly GIT_SHORT_COMMIT="$(git rev-parse --short HEAD)"
 readonly DOCKER_IMAGE_NAME="cluster.dev:${GIT_SHORT_COMMIT}-local-tests"
 
-docker build -t ${DOCKER_IMAGE_NAME} .
+docker build -t "${DOCKER_IMAGE_NAME}" .
 
 # Get from config.sh
 readonly USER="${AWS_ACCESS_KEY_ID}"
@@ -22,23 +22,23 @@ readonly TIMEOUT="${ACTION_TIMEOUT}"
 
 # Trap ctrl+c to remove docker container and kill timeout script.
 trap ctrl_c INT
-function ctrl_c() {
-    docker rm -f clusterdev-test-${GIT_SHORT_COMMIT}
-    kill ${timer_pid}
+function ctrl_c {
+    docker rm -f "clusterdev-test-${GIT_SHORT_COMMIT}"
+    kill "${timer_pid}"
 }
 
 # Script waits for $1 seconds and than remove $2 containet.
-${SRC_PATH}/tests/timeout.sh ${TIMEOUT} clusterdev-test-${GIT_SHORT_COMMIT} &
+${SRC_PATH}/tests/timeout.sh "${TIMEOUT}" "clusterdev-test-${GIT_SHORT_COMMIT}" &
 timer_pid=$!
 
 # Run docker in localhost
-docker run -d --name clusterdev-test-${GIT_SHORT_COMMIT} --workdir /github/workspace --rm -v "${SRC_PATH}":"/github/workspace" \
+docker run -d --name "clusterdev-test-${GIT_SHORT_COMMIT}" --workdir /github/workspace --rm -v "${SRC_PATH}:/github/workspace" \
            -e GITHUB_REPOSITORY="shalb" \
-           ${DOCKER_IMAGE_NAME} "${WORKFLOW_PATH}" "${USER}" "${PASS}"
+           "${DOCKER_IMAGE_NAME}" "${WORKFLOW_PATH}" "${USER}" "${PASS}"
 
 sleep 1
 
 # Show pipeline containet output.
-docker logs -f clusterdev-test-${GIT_SHORT_COMMIT}
+docker logs -f "clusterdev-test-${GIT_SHORT_COMMIT}"
 
 
