33abbc3326d312b4e94789cd0e9abc4cd64871e8
Volodymyr Tsap <voa@shalb.com>
2020-03-14 21:57:45 +0000

    Setting ArgoCD Apps function

--- bin/common.sh
+++ bin/common.sh
@@ -309,3 +309,20 @@ function main::output_software_info {
     INFO "AWS CLI"
     aws --version
 }
+
+#######################################
+# Deploy ArgoCD applications via kubectl
+# Globals:
+#   None
+# Arguments:
+#   None
+# Outputs:
+#   Writes progress status
+#######################################
+function argocd::deploy_apps {
+    DEBUG "Deploy ArgoCD apps via kubectl from kubernetes/apps folder"
+    DEBUG "Current folder $(pwd)"
+
+    INFO "Deploy Apps into ArgoCD"
+    run_cmd "kubectl apply -f kubernetes/apps --recursive"
+}
