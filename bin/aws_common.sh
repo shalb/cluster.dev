2882ce72a6570e6bc099d4de79a4bafba0334b9c
MaxymVlasov <MaxymVlasov@users.noreply.github.com>
2020-03-19 18:24:32 +0200

    Fix bash 'command not found' warning

--- bin/aws_common.sh
+++ bin/aws_common.sh
@@ -311,13 +311,13 @@ function aws::output_access_keys {
 
     KUBECONFIG_DOWNLOAD_MESSAGE="\
 Download and apply your kubeconfig using commands: \n\
-aws s3 cp s3://${CLUSTER_FULLNAME}/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/kubeconfig_${CLUSTER_FULLNAME} && \\ \n\
-export KUBECONFIG=\$KUBECONFIG:~/.kube/kubeconfig_${CLUSTER_FULLNAME} && \\ \n\
+aws s3 cp s3://${CLUSTER_FULLNAME}/kubeconfig_${CLUSTER_FULLNAME} ~/.kube/kubeconfig_${CLUSTER_FULLNAME} \n\
+export KUBECONFIG=\$KUBECONFIG:~/.kube/kubeconfig_${CLUSTER_FULLNAME} \n\
 kubectl get ns \n
 "
     SSH_ACCESS_MESSAGE="\
 Download your bastion ssh key using commands: \n\
-aws s3 cp s3://${CLUSTER_FULLNAME}/id_rsa_${CLUSTER_FULLNAME}.pem ~/.ssh/id_rsa_${CLUSTER_FULLNAME}.pem && chmod 600 ~/.ssh/id_rsa_${CLUSTER_FULLNAME}.pem && \\ \n\
+aws s3 cp s3://${CLUSTER_FULLNAME}/id_rsa_${CLUSTER_FULLNAME}.pem ~/.ssh/id_rsa_${CLUSTER_FULLNAME}.pem && chmod 600 ~/.ssh/id_rsa_${CLUSTER_FULLNAME}.pem \n\
 ssh -i ~/.ssh/id_rsa_${CLUSTER_FULLNAME}.pem centos@$CLUSTER_FULLNAME.$cluster_cloud_domain \n
 "
 
