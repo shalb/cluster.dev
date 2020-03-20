c1870ef3030fb83e6f15b02d43dda2bc29a23de2
Volodymyr Tsap <voa@shalb.com>
2020-03-20 15:34:59 +0000

    Fix comments from PR #32

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -19,9 +19,9 @@ function argocd::deploy_apps {
     INFO "Deploy Apps from /kubernetes/apps/<folder> into ArgoCD"
 
     for i in "${cluster_apps[@]}"; do
-    run_cmd "kubectl apply -f kubernetes/apps/$i --recursive";
+        run_cmd "kubectl apply -f kubernetes/apps/$i --recursive";
     done
 
-#TODO enable deletion from ArgoCD application that are installed but not mentioned in target folders manifests
+    #TODO: enable deletion from ArgoCD application that are installed but not mentioned in target folders manifests
 
 }
