d5d93e42e01b7b9b3b365cf67225702be0f8a132
romanprog <arti@auth.shalb.com>
2020-03-25 06:58:44 +0000

    commit for merge

--- terraform/aws/argocd/main.tf
+++ terraform/aws/argocd/main.tf
@@ -42,7 +42,7 @@ resource "helm_release" "argo-cd" {
   namespace  = "argocd"
 
   values = [
-    file("values.yaml")
+    file("./values.yaml")
   ]
   depends_on = [
     null_resource.kubeconfig_update,
@@ -51,7 +51,7 @@ resource "helm_release" "argo-cd" {
     name  = "server.certificate.domain"
     value = var.argo_domain
   }
-   set {
+  set {
     name  = "server.ingress.annotations.\"cluster\\.dev/domain\""
     value = var.argo_domain
   }
