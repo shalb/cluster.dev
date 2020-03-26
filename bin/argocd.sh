c41efabbe55c88b2f92621bcff6b94f568c5e6f4
Volodymyr Tsap <voa@shalb.com>
2020-03-26 08:35:52 +0000

    Add relative path

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -21,8 +21,8 @@ function argocd::deploy_apps {
     INFO "Deploy Apps from /kubernetes/apps/<folder> into ArgoCD"
 
     for ARGO_APP_DIR in "${cluster_apps_array[@]}"; do
-        run_cmd "kubectl apply -f /kubernetes/apps/$ARGO_APP_DIR --recursive" "" "false";
-        run_cmd "find /github/workspace"
+        run_cmd "kubectl apply -f /github/workspace/kubernetes/apps/$ARGO_APP_DIR --recursive" "" "false";
+        run_cmd "ls /github/workspace"
     done
 
     #TODO: enable deletion from ArgoCD application that are installed but not mentioned in target folders manifests
