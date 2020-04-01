e2f6516bf4dd3cefbb847a6a8d75e03487a4faa5
Volodymyr Tsap <voa@shalb.com>
2020-04-01 17:39:23 +0000

    Enable apps

--- entrypoint.sh
+++ entrypoint.sh
@@ -101,7 +101,7 @@ for CLUSTER_MANIFEST_FILE in $MANIFESTS; do
             kube::deploy_apps
 
             # Deploy Kubernetes Addons via Terraform
-            # aws::init_addons   "$cluster_name" "$cluster_cloud_region" "$cluster_cloud_domain"
+            aws::init_addons   "$cluster_name" "$cluster_cloud_region" "$cluster_cloud_domain"
 
             # Deploy ArgoCD via Terraform
             #aws::init_argocd   "$cluster_name" "$cluster_cloud_region" "$cluster_cloud_domain"
