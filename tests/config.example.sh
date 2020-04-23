44502186c9c66b22493d4a06e73ab7b4ffe9be0c
Volodymyr Tsap <voa@shalb.com>
2020-04-23 14:36:02 +0000

    Edit tests to work with ENV vars

--- tests/config.example.sh
+++ tests/config.example.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-AWS_ACCESS_KEY_ID=""
-AWS_SECRET_ACCESS_KEY=""
-
-ACTION_TIMEOUT="600" # Timeout in seconds. After timeout action will be terminated container will be stopped.
-CLUSTER_CONFIG_PATH="./.cluster.dev/gh-7.yaml" # Relative path from project root
+export AWS_ACCESS_KEY_ID="000000000000000000000" # aws_secret_access_key
+export AWS_SECRET_ACCESS_KEY="00000000000000000000000000000000000000" # aws_access_key_id
+export CLUSTER_CONFIG_PATH=".cluster.dev/aws-minikube.yaml" # Relative path from project root
+export ACTION_TIMEOUT="600" # Timeout in seconds. After timeout action will be terminated container will be stopped.
