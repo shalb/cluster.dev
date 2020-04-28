1b845db806e66ff64218894928501830c0dceee8
Volodymyr Tsap <voa@shalb.com>
2020-04-28 11:07:37 +0000

    Fix variables based on review

--- terraform/aws/route53/main.tf
+++ terraform/aws/route53/main.tf
@@ -21,12 +21,7 @@ resource "aws_route53_record" "sub-ns" {
   type            = "NS"
   ttl             = "300"
 
-  records = [
-    "${aws_route53_zone.sub.name_servers.0}",
-    "${aws_route53_zone.sub.name_servers.1}",
-    "${aws_route53_zone.sub.name_servers.2}",
-    "${aws_route53_zone.sub.name_servers.3}",
-  ]
+  records = aws_route53_zone.sub.name_servers
 }
 
 # Delegate created zone via lambda
@@ -34,8 +29,8 @@ resource "null_resource" "zone_delegation" {
   count = tobool(var.zone_delegation) ? 1 : 0
   # Update zone when DNS records are updated
   triggers = {
-    zone_id    = "${aws_route53_zone.sub.zone_id}"
-    dns_record = "${aws_route53_zone.sub.name_servers.0}"
+    zone_id    = aws_route53_zone.sub.zone_id
+    dns_record = aws_route53_zone.sub.name_servers.0
   }
   provisioner "local-exec" {
     command = <<EOF
--- terraform/aws/route53/outputs.tf
+++ terraform/aws/route53/outputs.tf
@@ -2,5 +2,5 @@ output "zone_id" {
   value = aws_route53_zone.sub.zone_id
 }
 output "name_servers" {
-  value = "${aws_route53_zone.sub.name_servers.0}, ${aws_route53_zone.sub.name_servers.1}, ${aws_route53_zone.sub.name_servers.2}, ${aws_route53_zone.sub.name_servers.3}"
+  value = aws_route53_zone.sub.name_servers
 }
