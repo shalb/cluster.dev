6652e3b8711399e54e156bb4930d835c5b068782
Volodymyr Tsap <voa@shalb.com>
2020-05-05 21:51:15 +0000

    fix to remove direcr repo path

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -18,10 +18,10 @@ function argocd::deploy_apps {
     DEBUG "Deploy ArgoCD apps via kubectl from kubernetes/apps folder"
     DEBUG "Current folder $(pwd)"
 
-    INFO "Deploy Apps from /kubernetes/apps/<folder> into ArgoCD"
+    INFO "Deploy Apps from ./kubernetes/apps/<folder> into ArgoCD"
 
     for ARGO_APP_DIR in "${cluster_apps_array[@]}"; do
-        run_cmd "kubectl apply -f $GIT_REPO_ROOT$ARGO_APP_DIR --recursive" "" "false";
+        run_cmd "kubectl apply -f .$ARGO_APP_DIR --recursive" "" "false";
     done
 
     #TODO: enable deletion from ArgoCD application that are installed but not mentioned in target folders manifests
