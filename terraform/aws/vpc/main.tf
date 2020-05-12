af6b036a82c6e87480a1c934f0173f2e5492abc5
Volodymyr Tsap <voa@shalb.com>
2020-05-12 15:23:58 +0000

    Add dynamic subnet creation

--- terraform/aws/vpc/main.tf
+++ terraform/aws/vpc/main.tf
@@ -16,9 +16,19 @@ module "vpc" {
 
   enable_dns_hostnames = true
   enable_dns_support   = true
-  public_subnets       = [cidrsubnet(var.vpc_cidr, 4, 0)]
+  public_subnets       = [for k, v in var.availability_zones : cidrsubnet(var.vpc_cidr, 4, k)]
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "owned"
+    "kubernetes.io/role/elb" = "1"
+  }
+  private_subnets = [for k, v in var.availability_zones : cidrsubnet(var.vpc_cidr, 4, k + 4)]
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = "1"
+  }
+  database_subnets = [for k, v in var.availability_zones : cidrsubnet(var.vpc_cidr, 4, k + 8)]
   tags = {
     Terraform = "true"
+    "cluster.dev-cluster_name" = var.cluster_name
   }
 }
 
