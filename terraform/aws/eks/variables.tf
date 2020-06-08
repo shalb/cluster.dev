6a9c9c8e9a10d564c402916a7bc04baa9d493ec6
Volodymyr Tsap <voa@shalb.com>
2020-06-08 16:11:54 +0300

    Update terraform/aws/eks/variables.tf
    Add type to subnets
    
    Co-authored-by: Maksym Vlasov <MaxymVlasov@users.noreply.github.com>

--- terraform/aws/eks/variables.tf
+++ terraform/aws/eks/variables.tf
@@ -13,6 +13,7 @@ variable "cluster_name" {
 
 variable "workers_subnets_type" {
   description = "Type of subnets to use on worker nodes: public or private"
+  type        = string
   default     = "private"
 }
 
