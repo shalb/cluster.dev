81b15006f68f936d13d034158f307ed8bc8d7977
Volodymyr Tsap <voa@shalb.com>
2020-06-10 15:17:15 +0000

    Add extra debug for bucket check

--- bin/aws_common.sh
+++ bin/aws_common.sh
@@ -50,8 +50,7 @@ function aws::is_s3_bucket_exists {
 
     # Create and init backend.
     run_cmd "terraform init"
-
-    terraform import -var="region=$cluster_cloud_region" -var="s3_backend_bucket=$S3_BACKEND_BUCKET" aws_s3_bucket.terraform_state "$S3_BACKEND_BUCKET" >/dev/null 2>&1
+    run_cmd "terraform import -var=region=$cluster_cloud_region -var=s3_backend_bucket=$S3_BACKEND_BUCKET aws_s3_bucket.terraform_state $S3_BACKEND_BUCKET"
     return $?
 }
 
