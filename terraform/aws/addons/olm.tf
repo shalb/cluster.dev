927c42cc4ade09344a3e25e37bd40a69c3904d81
Volodymyr Tsap <voa@shalb.com>
2020-06-24 12:30:50 +0000

    Fix OLM

--- terraform/aws/addons/olm.tf
+++ terraform/aws/addons/olm.tf
@@ -7,11 +7,12 @@ locals {
 
 resource "null_resource" "olm_install" {
   provisioner "local-exec" {
-    command = "kubectl apply -f ${local.url}/${local.olm-version}/crds.yaml -f ${local.url}/${local.olm-version}/olm.yaml && kubectl rollout status -w deployment/olm-operator -n ${local.olm-namespace}"
+    # Deploy CRD's, wait them become available and deploy olm
+    command = "kubectl apply -f ${local.url}/${local.olm-version}/crds.yaml && for resource in catalog-operator olm-operator packageserver; do kubectl rollout status -n ${local.olm-namespace} -w deploy $resource; done && kubectl apply -f ${local.url}/${local.olm-version}/olm.yaml"
   }
   provisioner "local-exec" {
     when    = destroy
-    command = "kubectl delete -f ${local.url}/${local.olm-version}/crds.yaml -f ${local.url}/${local.olm-version}/olm.yaml >/dev/null 2&>1"
+    command = "kubectl delete -f ${local.url}/${local.olm-version}/olm.yaml >/dev/null 2&>1"
   }
   depends_on = [
     null_resource.kubeconfig_update,
