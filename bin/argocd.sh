c44524902b5d82de49e861e1853703f129ce74de
Volodymyr Tsap <voa@shalb.com>
2020-07-10 16:46:42 +0000

    Debug ArgoCD Apps

--- bin/argocd.sh
+++ bin/argocd.sh
@@ -14,6 +14,8 @@ source "$PRJ_ROOT"/bin/logging.sh
 #######################################
 function argocd::deploy_apps {
     INFO "Deploy Apps from ./kubernetes/apps/<folder> into ArgoCD"
+    env
+    set
     for (( i=0; i<${cluster_apps_count}; i++ ));
         do
             local cluster_app_folder="";
