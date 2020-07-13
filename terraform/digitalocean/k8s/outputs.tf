9ce87fd6d527653efd370d8aaa2a9955e268d8ef
Volodymyr Tsap <voa@shalb.com>
2020-07-13 09:44:28 +0000

    FIxes by Review

--- terraform/digitalocean/k8s/outputs.tf
+++ terraform/digitalocean/k8s/outputs.tf
@@ -7,5 +7,6 @@ output "cluster_endpoint" {
 }
 
 output "kubernetes_config" {
+  # If first symbol defines absolute path (/) - form fullname to config, if not - use provided
   value = substr(var.config_output_path, -1, 1) == "/" ? "${var.config_output_path}kubeconfig_${var.cluster_name}" : var.config_output_path
 }
--- terraform/digitalocean/project/main.tf
+++
@@ -1,7 +0,0 @@
-resource "digitalocean_project" "project" {
-  name        = var.name
-  description = var.description
-  purpose     = var.purpose
-  environment = var.environment
-  resources   = var.resources
-}
