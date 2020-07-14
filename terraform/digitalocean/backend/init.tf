2b300cc7f2c18e582800f932d762a4a23c6bb2e4
Volodymyr Tsap <voa@shalb.com>
2020-07-14 14:55:08 +0000

    Fix DO

--- terraform/digitalocean/backend/init.tf
+++ terraform/digitalocean/backend/init.tf
@@ -5,3 +5,11 @@ terraform {
     digitalocean = "~> 1.18.0"
   }
 }
+
+provider digitalocean {
+
+  token             = var.do_token
+  spaces_access_id  = var.access_id
+  spaces_secret_key = var.secret_key
+
+}
