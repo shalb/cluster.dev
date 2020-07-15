1e2dba3f32fc37c742b242ce344c9aae07533267
Volodymyr Tsap <voa@shalb.com>
2020-07-15 03:40:52 +0000

    Edit Region

--- terraform/digitalocean/backend/init.tf
+++ terraform/digitalocean/backend/init.tf
@@ -5,3 +5,8 @@ terraform {
     digitalocean = "~> 1.18.0"
   }
 }
+
+# Despite DO region should be set in us-east-1: https://github.com/aws/aws-sdk-go/issues/2232#issuecomment-434838388
+provider digitalocean {
+    region                      = "us-east-1"
+}
