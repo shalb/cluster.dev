df4e3ae323c3805de2fd834fa3e6fb14579cf40d
romanprog <arti@shalb.com>
2021-01-21 09:14:22 +0000

    do tf modules terraform verrsion

--- terraform/digitalocean/domain/init.tf
+++ terraform/digitalocean/domain/init.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 0.12.0"
+  required_version = "~> 0.13.0"
   backend "s3" {
     region                      = "us-east-1"
     skip_credentials_validation = true
