b3ea99f37322718d9e90c4abf4ad7813734bf8f8
romanprog <arti@shalb.com>
2023-08-22 16:34:58 +0300

    remove debug output, fix yaml parse

--- pkg/project/scanners.go
+++ pkg/project/scanners.go
@@ -70,7 +70,6 @@ func OutputsReplacer(data reflect.Value, unit Unit) (reflect.Value, error) {
 				} else {
 					dataStr = link.OutputData.(string)
 				}
-				log.Warnf("OutputsReplacer: replace dataStr %v", dataStr)
 				resString = strings.ReplaceAll(resString, marker, dataStr)
 			}
 		}
