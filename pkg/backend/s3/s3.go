4cefbb6d7cd4d5151d2290b90a33b132663a2e9d
romanprog <arti@shalb.com>
2023-09-15 12:14:16 +0300

    minio support

--- pkg/backend/s3/s3.go
+++ pkg/backend/s3/s3.go
@@ -9,19 +9,23 @@ import (
 	"time"
 
 	"github.com/apex/log"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/feature/ec2/imds"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	awsbase "github.com/hashicorp/aws-sdk-go-base/v2"
 	"github.com/hashicorp/hcl/v2/hclwrite"
 	"github.com/shalb/cluster.dev/pkg/config"
 	"github.com/shalb/cluster.dev/pkg/project"
+	"github.com/shalb/cluster.dev/pkg/utils"
 	"github.com/zclconf/go-cty/cty"
+	"gopkg.in/yaml.v3"
 )
 
 // Backend - describe s3 backend for interface package.backend.
 type Backend struct {
-	name     string
-	s3Client *s3.Client
+	name     string     `yaml:"-"`
+	s3Client *s3.Client `yaml:"-"`
 
 	Bucket                    string `yaml:"bucket"`
 	Region                    string `yaml:"region"`
@@ -37,6 +41,7 @@ type Backend struct {
 	SkipRegionValidation      string `yaml:"skip_region_validation,omitempty"`
 	StsEndpoint               string `yaml:"sts_endpoint,omitempty"`
 	IamEndpoint               string `yaml:"iam_endpoint,omitempty"`
+	ForcePathStyle            bool   `yaml:"force_path_style,omitempty"`
 	// Assume role
 	AssumeRoleDurationSeconds   string            `yaml:"assume_role_duration_seconds,omitempty"`
 	AssumeRolePolicy            string            `yaml:"assume_role_policy,omitempty"`
@@ -46,11 +51,11 @@ type Backend struct {
 	ExternalId                  string            `yaml:"external_id,omitempty"`
 	RoleArn                     string            `yaml:"role_arn,omitempty"`
 	SessionName                 string            `yaml:"session_name,omitempty"`
-	//
+
 	Workspaces string `yaml:"workspaces,omitempty"`
 
-	ProjectPtr *project.Project
-	state      map[string]interface{}
+	ProjectPtr *project.Project       `yaml:"-"`
+	state      map[string]interface{} `yaml:"-"`
 }
 
 func (b *Backend) State() map[string]interface{} {
@@ -115,6 +120,12 @@ func (b *Backend) Configure() error {
 		SharedCredentialsFiles: sharedCredentialsFiles,
 		APNInfo:                stdUserAgentProducts(),
 	}
+	if b.SkipMetadataApiCheck {
+		cfg.EC2MetadataServiceEnableState = imds.ClientDisabled
+	} else {
+		cfg.EC2MetadataServiceEnableState = imds.ClientEnabled
+	}
+
 	ctx := context.TODO()
 	_, awsConfig, diags := awsbase.GetAwsConfig(ctx, cfg)
 
@@ -126,7 +137,15 @@ func (b *Backend) Configure() error {
 		return fmt.Errorf(diagError)
 	}
 
-	b.s3Client = s3.NewFromConfig(awsConfig)
+	b.s3Client = s3.NewFromConfig(awsConfig,
+		func(o *s3.Options) {
+			if b.ForcePathStyle {
+				o.UsePathStyle = true
+			}
+			if b.Endpoint != "" {
+				o.BaseEndpoint = aws.String(b.Endpoint)
+			}
+		})
 	return nil
 }
 
@@ -146,13 +165,34 @@ func (b *Backend) GetBackendHCL(stackName, unitName string) (*hclwrite.File, err
 	terraformBlock := rootBody.AppendNewBlock("terraform", []string{})
 	backendBlock := terraformBlock.Body().AppendNewBlock("backend", []string{"s3"})
 	backendBody := backendBlock.Body()
-	backendBody.SetAttributeValue("bucket", cty.StringVal(b.Bucket))
+	// backendBody.SetAttributeValue("bucket", cty.StringVal(b.Bucket))
 	backendBody.SetAttributeValue("key", cty.StringVal(fmt.Sprintf("%s/%s.state", stackName, unitName)))
-	backendBody.SetAttributeValue("region", cty.StringVal(b.Region))
-
-	// terraformBlock.Body().SetAttributeValue("required_version", cty.StringVal("~> 0.13"))
+	// backendBody.SetAttributeValue("region", cty.StringVal(b.Region))
+	terraformBlock.Body().SetAttributeValue("required_version", cty.StringVal("~> 0.13"))
+	bkMap, err := getBackendMap(*b)
+	if err != nil {
+		return nil, err
+	}
+	for key, val := range bkMap {
+		backendBody.SetAttributeValue(key, val)
+	}
 	return f, nil
 
+}
+
+func getBackendMap(in Backend) (res map[string]cty.Value, err error) {
+	tmpData, err := yaml.Marshal(in)
+	if err != nil {
+		return
+	}
+	resMap := map[string]interface{}{}
+	err = yaml.Unmarshal(tmpData, &resMap)
+	err = utils.ResolveYamlError(tmpData, err)
+	res = map[string]cty.Value{}
+	for k, v := range resMap {
+		res[k] = cty.StringVal(fmt.Sprintf("%v", v))
+	}
+	return
 }
 
 // GetRemoteStateHCL generate terraform remote state for this backend.
@@ -163,16 +203,18 @@ func (b *Backend) GetRemoteStateHCL(stackName, unitName string) ([]byte, error) 
 	dataBlock := rootBody.AppendNewBlock("data", []string{"terraform_remote_state", fmt.Sprintf("%s-%s", stackName, unitName)})
 	dataBody := dataBlock.Body()
 	dataBody.SetAttributeValue("backend", cty.StringVal("s3"))
-	dataBody.SetAttributeValue("config", cty.MapVal(map[string]cty.Value{
-		"bucket": cty.StringVal(b.Bucket),
-		"key":    cty.StringVal(fmt.Sprintf("%s/%s.state", stackName, unitName)),
-		"region": cty.StringVal(b.Region),
-	}))
+	config, err := getBackendMap(*b)
+	if err != nil {
+		return nil, fmt.Errorf("generate s3 remote state: %w", err)
+	}
+	config["key"] = cty.StringVal(fmt.Sprintf("%s/%s.state", stackName, unitName))
 
+	dataBody.SetAttributeValue("config", cty.MapVal(config))
 	return f.Bytes(), nil
 }
 
 func (b *Backend) ReadState() (string, error) {
+	log.Warnf("ReadState 1")
 	result, err := b.s3Client.GetObject(
 		context.TODO(),
 		&s3.GetObjectInput{
@@ -181,6 +223,10 @@ func (b *Backend) ReadState() (string, error) {
 		},
 	)
 	if err != nil {
+		var bne *types.NoSuchKey
+		if errors.As(err, &bne) {
+			return "", nil
+		}
 		return "", fmt.Errorf("get state from s3 bucket: %v", err.Error())
 	}
 	defer result.Body.Close()
@@ -228,14 +274,15 @@ func (b *Backend) LockState() error {
 		},
 	)
 	if err != nil {
-		var bne *types.NotFound
+		var bne *types.NoSuchKey
 		if !errors.As(err, &bne) {
 			return fmt.Errorf("lock state: get lock file: %v", err.Error())
 		}
+	} else {
+		defer result.Body.Close()
 	}
-	defer result.Body.Close()
 	if err == nil {
-		return fmt.Errorf("lock state file found, the state is locked")
+		return fmt.Errorf("lock state file found, the state is locked. Use command 'cdev state unlock' to force unlock (unsafe)")
 	}
 	// Lock file not found, process lock
 	ioBody := strings.NewReader(b.ProjectPtr.SessionId)
@@ -244,11 +291,11 @@ func (b *Backend) LockState() error {
 		context.TODO(),
 		&s3.PutObjectInput{
 			Bucket: &b.Bucket,
-			Key:    b.stateKey(),
+			Key:    b.lockKey(),
 			Body:   ioBody,
 		},
 	)
-	if err == nil {
+	if err != nil {
 		return fmt.Errorf("write state to s3 bucket: %v", err.Error())
 	}
 	return nil
@@ -263,7 +310,7 @@ func (b *Backend) UnlockState() error {
 			Key:    b.lockKey(),
 		},
 	)
-	if err == nil {
+	if err != nil {
 		return fmt.Errorf("delete state lock from s3 bucket: %v", err.Error())
 	}
 	return nil
